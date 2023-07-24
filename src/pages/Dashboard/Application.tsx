12d081efb8759dd254445854db0f9137a5361284
samuel-akinboro <victorakinboro2003@gmail.com>
2023-07-24 08:20:01 +0100

    table in progress

--- src/pages/Dashboard/Application.tsx
+++ src/pages/Dashboard/Application.tsx
@@ -16,6 +16,38 @@ const Application = () => {
                 Static Billboard
                 <ChevronDownIcon className="h-5 text-[#6C727F]" />
               </p>
+              <table className="bg-white border-collapse w-full mt-8">
+                <tr className="font-medium">
+                  <th className="text-left px-8 py-4">ID</th>
+                  <th className="text-left px-8 py-4">Description</th>
+                  <th className="text-left px-8 py-4">- .</th>
+                  <th className="text-left px-8 py-4">Date Added</th>
+                  <th className="text-left px-8 py-4"></th>
+                  <th className="text-left px-8 py-4"> </th>
+                </tr>
+                <tr
+                  className="hover:bg-gray-50 focus:bg-gray-300 active:bg-red-200 border-b border-gray3 text-[#1D2130] font-light"
+                  tabIndex={0}
+                >
+                  <td className="px-8 py-6">QWK00</td>
+                  <td className="px-8 py-6">Alfreds Futterkiste</td>
+                  <td className="px-8 py-6">Dante Sparks</td>
+                  <td className="px-8 py-6">May 16, 2023</td>
+                  <td className="px-8 py-6">Lekki Lagos</td>
+                  <td className="px-8 py-6">View details</td>
+                </tr>
+                <tr
+                  className="hover:bg-gray-50 focus:bg-gray-300 active:bg-red-200 border-b border-gray3 text-[#1D2130] font-light"
+                  tabIndex={0}
+                >
+                  <td className="px-8 py-6">QWK00</td>
+                  <td className="px-8 py-6">Alfreds Futterkiste</td>
+                  <td className="px-8 py-6">Dante Sparks</td>
+                  <td className="px-8 py-6">May 16, 2023</td>
+                  <td className="px-8 py-6">Lekki Lagos</td>
+                  <td className="px-8 py-6">View details</td>
+                </tr>
+              </table>
             </div>
           </div>
         </MainArea>
